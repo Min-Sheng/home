0b6d82b61f54080fbd4568cb6a9dfee6681a2d1f
吳旻昇 <vincentwu@wuminshengdeMacBook-Pro.local>
2021-03-31 20:59:58 +0800

    Updates

---
+++ precache-manifest.4c1c4a729e86342e7811eb9dc99ae2b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016d469eac68de897a3df3b562ba6c69",
+    "url": "//min-sheng.github.io/index.html"
+  },
+  {
+    "revision": "8821c99ca297a6b00b05",
+    "url": "//min-sheng.github.io/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "//min-sheng.github.io/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "//min-sheng.github.io/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8821c99ca297a6b00b05",
+    "url": "//min-sheng.github.io/static/js/main.fea39ab6.chunk.js"
+  },
+  {
+    "revision": "242d0d6a23e32345bd32",
+    "url": "//min-sheng.github.io/static/js/runtime-main.3ca8dc26.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "//min-sheng.github.io/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "//min-sheng.github.io/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e39bc3842dbc3d10fbad9c9b3d87720e.js"
+  "//min-sheng.github.io/precache-manifest.4c1c4a729e86342e7811eb9dc99ae2b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//min-sheng.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
